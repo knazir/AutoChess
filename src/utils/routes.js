b658516c1591b6d1ea284fb647a8dab5c47c4502
Kashif Nazir <knazir@stanford.edu>
2019-05-24 20:52:33 -0700

    Try to fix link issue with GitHub pages, add GitHub data json

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,6 +1,14 @@
+import github from "../data/github";
+
+const prefix = process.env.NODE_ENV === "development" ? "/" : github.prefix;
+
+function route(path) {
+  return `${prefix}${path}`;
+}
+
 const routes = {
-  about: "/",
-  rerolls: "/rerolls"
+  about: route(""),
+  rerolls: route("rerolls")
 };
 
 export default routes;
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import utils from "../utils/helpers";
+import github from "../data/github.json";
 import "../styles/about.scss";
 
 export default class About extends Component {
@@ -8,13 +9,13 @@ export default class About extends Component {
       <div className="about view">
         <img className="autoChessLogo" src={utils.imgUrl("auto_chess.jpg")} alt="AutoChess Logo"/>
         <p>
-          Hi, <a href="https://www.github.com/knazir" target="_blank" rel="noopener noreferrer">I'm</a> really bad
-          at AutoChess, but I think it's really fun. I started working on some tools to help me make better decisions
-          while playing. I hope that you may find them useful as well.
+          Hi, <a href={github.profile} target="_blank" rel="noopener noreferrer">I'm</a> really bad at AutoChess,
+          but I think it's really fun. I started working on some tools to help me make better decisions while
+          playing. I hope that you may find them useful as well.
         </p>
         <p>
           You can find the source for these tools and submit bugs
-          on <a href="https://www.github.com/knazir/AutoChess" target="_blank" rel="noopener noreferrer">GitHub</a>.
+          on <a href={github.repo} target="_blank" rel="noopener noreferrer">GitHub</a>.
         </p>
         <p>
           I did not make AutoChess. All images and information on this site are copyright their respective
