f7f1d38a35418e75b51788a3e66910f5f8aeed43
Kashif Nazir <knazir@stanford.edu>
2019-07-09 19:09:09 -0700

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/* Google Analytics */
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+  window.dataLayer.push(arguments);
+}
+try {
+  gtag("js", new Date());
+  gtag("config", "UA-143604066-1");
+} catch (e) {
+  console.log(`Analytics disabled: ${e.message}`);
+}
